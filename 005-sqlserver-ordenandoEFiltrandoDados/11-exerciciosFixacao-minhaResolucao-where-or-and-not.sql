8e1896fde0b66a4c5b1e07d16df31d15cfbfc04e
Jorge Harbes <jharbes@hotmail.com>
2023-07-23 16:23:16 -0300

    Update 11-exerciciosFixacao-minhaResolucao-where-or-and-not.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/11-exerciciosFixacao-minhaResolucao-where-or-and-not.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/11-exerciciosFixacao-minhaResolucao-where-or-and-not.sql
@@ -1,7 +1,9 @@
 -- Exercicios de Fixacao: WHERE, AND, OR e NOT
 
--- 1- Selecione todas as linhas da tabela dimEmployee de funcionários do sexo feminino E do
--- departamento de Finanças:
+/*
+1- Selecione todas as linhas da tabela dimEmployee de funcionários do sexo feminino E do
+departamento de Finanças:
+*/
 
 select 
 	* 
@@ -9,3 +11,17 @@ from
 	DimEmployee
 where
 	Gender='F' and DepartmentName = 'Finance';
+
+
+
+/*
+2- Selecione todas as linhas da tabela DimProduct de produtos da marca Contoso E da cor vermelha
+E que tenham um UnitPrice maior ou igual a $100.
+*/
+
+select
+	*
+from
+	DimProduct
+where
+	BrandName='Contoso' and ColorName='Red' and UnitPrice>=100;
