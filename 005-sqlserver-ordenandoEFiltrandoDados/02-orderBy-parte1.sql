57a696a7c7510ab09d5a2ec818de1108fcef490a
Jorge Harbes <jharbes@hotmail.com>
2023-07-23 12:56:24 -0300

    Update 02-orderBy-parte1.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/02-orderBy-parte1.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/02-orderBy-parte1.sql
@@ -2,5 +2,10 @@
 
 -- Selecionando as TOP 100 linhas da tabela DimStore, ordenando pela coluna EmployeeCount de forma decrescente
 
+-- Por default o ORDER BY ordernará de forma ascendente (pode ser usado também ORDER BY <nome_da_coluna> ASC), para que
+-- seja feita de forma descendente teremos que usar ORDER BY <nome_da_coluna> DESC
+
+use ContosoRetailDW;
+
 SELECT TOP(100) * FROM DimStore
-ORDER BY EmployeeCount DESC
+ORDER BY EmployeeCount DESC;
