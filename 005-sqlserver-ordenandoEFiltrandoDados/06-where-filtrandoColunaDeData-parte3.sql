adc6c236cec649b2054c95e835c79d42cd424f26
Jorge Harbes <jharbes@hotmail.com>
2023-07-23 14:16:59 -0300

    Update 06-where-filtrandoColunaDeData-parte3.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/06-where-filtrandoColunaDeData-parte3.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/06-where-filtrandoColunaDeData-parte3.sql
@@ -3,7 +3,9 @@
 
 -- Podemos filtrar os dados nas nossas tabelas utilizando o comando WHERE
 
--- Quais clientes nasceram após o dia 31/12/1970?
+-- Formato de data no SQL será em 'yyyy-mm-dd'
+
+-- Quais clientes nasceram após o dia 31/12/1970 ordenando do mais novo para o mais velho?
 
 SELECT * FROM DimCustomer
 WHERE BirthDate >= '1970-12-31'
