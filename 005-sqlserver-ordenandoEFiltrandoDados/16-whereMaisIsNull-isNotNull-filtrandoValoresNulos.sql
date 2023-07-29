52e28cfa29bf3460613a321780559f687c2d5842
Jorge Harbes <jharbes@hotmail.com>
2023-07-29 15:09:44 -0300

    Update 16-whereMaisIsNull-isNotNull-filtrandoValoresNulos.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/16-whereMaisIsNull-isNotNull-filtrandoValoresNulos.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/16-whereMaisIsNull-isNotNull-filtrandoValoresNulos.sql
@@ -4,4 +4,22 @@
 -- 1. Selecione os clientes que são pessoa física.
 
 SELECT * FROM DimCustomer
-WHERE CompanyName IS NULL
+WHERE CompanyName IS NULL;
+
+
+-- 2. Selecione os clientes que são pessoa jurídica.
+
+SELECT * FROM DimCustomer
+WHERE CompanyName IS NOT NULL;
+
+
+-- 3. Selecione os funcionários que estão em atividade
+
+SELECT * FROM DimEmployee
+WHERE EndDate IS NULL;
+
+
+-- 4. Selecione os funcionários que NÃO estão em atividade
+
+SELECT * FROM DimEmployee
+WHERE EndDate IS NOT NULL;
