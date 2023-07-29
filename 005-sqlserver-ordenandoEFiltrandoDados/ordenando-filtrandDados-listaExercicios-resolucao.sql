fcf1b0262185d5a4c8fc1585b4d778a2a62209ed
Jorge Harbes <jharbes@hotmail.com>
2023-07-29 15:25:04 -0300

    Create ordenando-filtrandDados-listaExercicios-resolucao.sql

---
+++ 005-sqlserver-ordenandoEFiltrandoDados/ordenando-filtrandDados-listaExercicios-resolucao.sql
@@ -0,0 +1,16 @@
+-- Exercicios - ordenando-filtrandoDados
+
+use ContosoRetailDW;
+
+/*
+1. Você é responsável por controlar os dados de clientes e de produtos da sua empresa.
+O que você precisará fazer é confirmar se:
+
+a. Existem 2.517 produtos cadastrados na base e, se não tiver, você deverá reportar ao seu
+gestor para saber se existe alguma defasagem no controle dos produtos.
+
+b. Até o mês passado, a empresa tinha um total de 19.500 clientes na base de controle.
+Verifique se esse número aumentou ou reduziu.
+*/
+
+-- a)
