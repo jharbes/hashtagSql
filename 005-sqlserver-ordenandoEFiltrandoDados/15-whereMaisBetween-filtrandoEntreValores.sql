17b01906caf3a4f359a1f2d6e1b9877773e49526
Jorge Harbes <jharbes@hotmail.com>
2023-07-29 14:57:53 -0300

    Update 15-whereMaisBetween-filtrandoEntreValores.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/15-whereMaisBetween-filtrandoEntreValores.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/15-whereMaisBetween-filtrandoEntreValores.sql
@@ -4,4 +4,16 @@
 -- 1. Selecione os funcionários que têm a data de contratação entre '01-01-2000' e '31-12-2000'.
 
 SELECT * FROM DimEmployee
-WHERE HireDate BETWEEN '2000-01-01' AND '2000-12-31'
+WHERE HireDate BETWEEN '2000-01-01' AND '2000-12-31';
+
+
+SELECT * FROM DimEmployee
+WHERE EmployeeKey BETWEEN 223 AND 229;
+
+
+SELECT * FROM DimProduct
+WHERE UnitPrice BETWEEN 50 AND 100;
+
+
+SELECT * FROM DimProduct
+WHERE UnitPrice NOT BETWEEN 50 AND 100;
