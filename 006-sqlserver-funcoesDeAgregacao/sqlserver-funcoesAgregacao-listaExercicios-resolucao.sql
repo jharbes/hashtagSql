05298983f6ecebceb6da6d0403418db2ac564d58
Jorge Harbes <jharbes@hotmail.com>
2023-07-30 17:10:03 -0300

    Create sqlserver-funcoesAgregacao-listaExercicios-resolucao.sql

---
+++ 006-sqlserver-funcoesDeAgregacao/sqlserver-funcoesAgregacao-listaExercicios-resolucao.sql
@@ -0,0 +1,12 @@
+-- Exercicios - funcoesAgregacao
+
+use ContosoRetailDW;
+
+/*
+1. O gerente comercial pediu a você uma análise da Quantidade Vendida e Quantidade
+Devolvida para o canal de venda mais importante da empresa: Store.
+
+Utilize uma função SQL para fazer essas consultas no seu banco de dados. Obs: Faça essa
+análise considerando a tabela FactSales.
+*/
+
