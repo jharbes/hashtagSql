26e739298bd7642cd2b1f370ccac4e9ef5216ca2
Jorge Harbes <jharbes@hotmail.com>
2023-07-30 16:08:28 -0300

    Create 01-introducao.sql

---
+++ 006-sqlserver-funcoesDeAgregacao/01-introducao.sql
@@ -0,0 +1,25 @@
+/*
+
+SQL Server - Funções de Agregação
+
+01-introducao
+
+
+São as funções que agregam os dados, por exemplo:
+
+COUNT - Contagem de valores
+
+MIN - Retorna o valor mínimo da coluna
+
+MAX - Retorna o valor máximo da coluna
+
+DISTINCT - Retorna os valores distintos
+
+COUNT + DISTINCT - Contagem de valores distintos
+
+AVG - Retorna a média dos valores
+
+SUM - Retorna o somatório dos valores
+
+
+*/
