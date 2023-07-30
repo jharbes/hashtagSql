3e1da5b25b09cba32e2e17233cf5d1876db0a2f0
Jorge Harbes <jharbes@hotmail.com>
2023-07-30 14:58:21 -0300

    Update ordenando-filtrandDados-listaExercicios-resolucao.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/ordenando-filtrandDados-listaExercicios-resolucao.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/ordenando-filtrandDados-listaExercicios-resolucao.sql
@@ -113,3 +113,23 @@ from
 	DimStore
 where
 	Status='On' and StoreType='Store';
+
+
+
+/*
+5. O gerente da área de controle de qualidade notificou à Contoso que todos os produtos Home
+Theater da marca Litware, disponibilizados para venda no dia 15 de março de 2009, foram
+identificados com defeitos de fábrica.
+
+O que você deverá fazer é identificar os ID’s desses produtos e repassar ao gerente para que ele
+possa notificar as lojas e consequentemente solicitar a suspensão das vendas desses produtos.
+*/
+
+-- observe que a consulta teve seus valores na coluna de DATETIME (data e hora) alterados para
+-- que a consulta pudesse retornar corretamente, tivemos que tirar os traços (-) da data.
+select 
+	*
+from
+	DimProduct
+where
+	BrandName = 'Litware' AND ProductName like '%Home Theater%' and AvailableForSaleDate='20090315';
