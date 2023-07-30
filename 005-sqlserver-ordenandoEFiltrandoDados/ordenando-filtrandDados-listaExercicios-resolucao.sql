de4a91432513f581e0caf62a219080e6957694be
Jorge Harbes <jharbes@hotmail.com>
2023-07-30 15:54:10 -0300

    Update ordenando-filtrandDados-listaExercicios-resolucao.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/ordenando-filtrandDados-listaExercicios-resolucao.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/ordenando-filtrandDados-listaExercicios-resolucao.sql
@@ -165,3 +165,19 @@ from
 	DimProduct
 where
 	ProductDescription like '%LCD%';
+
+
+
+/*
+9. Faça uma lista com todos os produtos das cores: Green, Orange, Black, Silver e Pink. Estes
+produtos devem ser exclusivamente das marcas: Contoso, Litware e Fabrikam.
+*/
+
+select
+	*
+from
+	DimProduct
+where
+		ColorName in ('Green','Orange','Black','Pink','Silver')
+	and
+		BrandName in ('Contoso','Litware','Fabrikam');
