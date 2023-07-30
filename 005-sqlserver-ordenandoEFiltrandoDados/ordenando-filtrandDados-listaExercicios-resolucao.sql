47d3a8604f49ec86a703096cbc7e106e16efcb50
Jorge Harbes <jharbes@hotmail.com>
2023-07-30 16:04:31 -0300

    Update ordenando-filtrandDados-listaExercicios-resolucao.sql

--- 005-sqlserver-ordenandoEFiltrandoDados/ordenando-filtrandDados-listaExercicios-resolucao.sql
+++ 005-sqlserver-ordenandoEFiltrandoDados/ordenando-filtrandDados-listaExercicios-resolucao.sql
@@ -181,3 +181,19 @@ where
 		ColorName in ('Green','Orange','Black','Pink','Silver')
 	and
 		BrandName in ('Contoso','Litware','Fabrikam');
+
+
+
+/*
+10. A empresa possui 16 produtos da marca Contoso, da cor Silver e com um UnitPrice entre 10 e
+30. Descubra quais são esses produtos e ordene o resultado em ordem decrescente de acordo
+com o preço (UnitPrice).
+*/
+
+select
+	*
+from
+	DimProduct
+where
+	BrandName='Contoso' and ColorName='Silver' and UnitPrice between 10 and 30
+order by UnitPrice desc;
