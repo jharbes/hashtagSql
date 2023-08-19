4a23b2b26dc780f935b1ec403a97ba9364776c9a
Jorge Harbes <jharbes@hotmail.com>
2023-08-19 14:07:09 -0300

    Update 06-groupBy-having.sql

--- 007-sqlserver-criandoAgrupamentosNoSql/06-groupBy-having.sql
+++ 007-sqlserver-criandoAgrupamentosNoSql/06-groupBy-having.sql
@@ -12,3 +12,20 @@ GROUP BY BrandName
 
 HAVING COUNT(BrandName) >= 200
 
+
+-- IMPORTANTE**: nao é possivel utilizar o comando where para fazer esse tipo
+-- de filtro, pois o comando WHERE so funciona ANTES do group by, após o group by
+-- só funcionara o HAVING
+
+
+SELECT
+	BrandName AS 'Marca',
+	COUNT(BrandName) AS 'Total Marca'
+FROM
+	DimProduct
+GROUP BY
+	BrandName
+HAVING
+	COUNT(BrandName) >= 200
+ORDER BY
+	'Total Marca' desc;
