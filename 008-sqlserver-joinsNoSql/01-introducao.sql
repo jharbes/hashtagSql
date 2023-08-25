2b26688e6553c44a58afc3d5fdd118c901aeac74
Jorge Harbes <jharbes@hotmail.com>
2023-08-25 17:38:33 -0300

    Create 01-introducao.sql

---
+++ 008-sqlserver-joinsNoSql/01-introducao.sql
@@ -0,0 +1,29 @@
+/*
+
+SQL Server - JOINs no SQL
+
+01-introducao
+
+São os comandos que criam RELAÇÕES entre as tabelas do nosso banco de dados.
+
+- Chave primária vs Chave estrangeira
+
+- Tabela fato vs Tabela dimensao
+
+- Tipos de JOIN
+
+- INNER JOIN
+
+- LEFT JOIN
+
+- RIGHT JOIN
+
+- FULL JOIN
+
+- CROSS JOIN
+
+- Multiplos JOIN
+
+- UNION e UNION ALL
+
+*/
