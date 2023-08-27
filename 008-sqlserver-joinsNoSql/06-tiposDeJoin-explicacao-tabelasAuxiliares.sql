8febfbf2b58b9211f1a3b1132c03b13c3b9270c4
Jorge Harbes <jharbes@hotmail.com>
2023-08-27 11:45:42 -0300

    Create 06-tiposDeJoin-explicacao-tabelasAuxiliares.sql

---
+++ 008-sqlserver-joinsNoSql/06-tiposDeJoin-explicacao-tabelasAuxiliares.sql
@@ -0,0 +1,36 @@
+/*
+CÓDIGO PARA CRIAR AS TABELAS AUXILIARES PARA AS EXPLICAÇÕES:
+
+CRIE 2 CONSULTAS E EM CADA UMA DELAS, EXECUTE OS SEGUINTES COMANDOS:
+
+Consulta 1:
+*/
+
+CREATE TABLE produtos(
+id_produto int,
+nome_produto varchar(30),
+id_subcategoria int)
+
+INSERT INTO produtos(id_produto, nome_produto, id_subcategoria)
+VALUES
+	(1, 'Fone Bluetooth JBL', 5),
+	(2, 'PS4', 6),
+	(3, 'Notebook Samsung i3', 2),
+	(4, 'iPhone 11', 1),
+	(5, 'Moto G9', 1)
+
+---
+
+-- Consulta 2:
+
+CREATE TABLE subcategoria(
+id_subcategoria int,
+nome_subcategoria varchar(30))
+
+INSERT INTO subcategoria(id_subcategoria, nome_subcategoria)
+VALUES
+	(1, 'Celular'),
+	(2, 'Notebook'),
+	(3, 'Câmera Digital'),
+	(4, 'Televisão'),
+	(5, 'Fone de Ouvido')
