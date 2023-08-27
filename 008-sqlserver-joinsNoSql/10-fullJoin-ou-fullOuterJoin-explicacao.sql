28024ea27166a5fe51be4ef084b1bbdd46bb3326
Jorge Harbes <jharbes@hotmail.com>
2023-08-27 14:48:25 -0300

    Create 10-fullJoin-ou-fullOuterJoin-explicacao.sql

---
+++ 008-sqlserver-joinsNoSql/10-fullJoin-ou-fullOuterJoin-explicacao.sql
@@ -0,0 +1,19 @@
+-- 10-fullJoin-ou-fullOuterJoin-explicacao
+
+
+use ContosoRetailDW;
+
+select * from produtos;
+select * from subcategoria;
+
+
+
+select
+	id_produto,
+	nome_produto,
+	produtos.id_subcategoria,
+	subcategoria.nome_subcategoria
+from
+	produtos
+full join
+	subcategoria on produtos.id_subcategoria=subcategoria.id_subcategoria;
