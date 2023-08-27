055c859ac7ce2489f7419514f7004550cd50c74a
Jorge Harbes <jharbes@hotmail.com>
2023-08-27 14:19:57 -0300

    Create 07-leftJoin-ou-leftOuterJoin-explicacao.sql

---
+++ 008-sqlserver-joinsNoSql/07-leftJoin-ou-leftOuterJoin-explicacao.sql
@@ -0,0 +1,24 @@
+-- 07-leftJoin-ou-leftOuterJoin-explicacao
+
+use ContosoRetailDW;
+
+select * from produtos;
+select * from subcategoria;
+
+
+-- retorna TODOS os itens da tabela à esquerda, e da tabela à direita
+-- apenas os itens EM COMUM com a tabela da esquerda 
+select * from produtos left join subcategoria on produtos.id_subcategoria=subcategoria.id_subcategoria;
+select * from produtos left outer join subcategoria on produtos.id_subcategoria=subcategoria.id_subcategoria;
+
+select
+	produtos.id_produto,
+	produtos.nome_produto,
+	produtos.id_subcategoria,
+	subcategoria.nome_subcategoria
+from
+	produtos
+left join
+	subcategoria
+on
+	produtos.id_subcategoria=subcategoria.id_subcategoria;
