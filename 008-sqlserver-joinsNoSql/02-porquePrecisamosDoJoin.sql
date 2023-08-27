fcd132ee2af7b764ef3e29efee3aa06e74127b69
Jorge Harbes <jharbes@hotmail.com>
2023-08-27 10:21:52 -0300

    Update 02-porquePrecisamosDoJoin.sql

--- 008-sqlserver-joinsNoSql/02-porquePrecisamosDoJoin.sql
+++ 008-sqlserver-joinsNoSql/02-porquePrecisamosDoJoin.sql
@@ -8,4 +8,9 @@ select * from DimChannel;
 
 
 -- usaremos o JOIN Para relacionar duas ou mais colunas atraves de colunas que tenham algum valor em comum
+
+-- left join preferencia para listar as informacoes da tabela da esquerda
 select top(10) * from FactSales left join DimChannel on FactSales.channelKey=DimChannel.ChannelKey;
+
+-- right join preferencia para listar as informacoes da tabela da esquerda
+select top(10) * from FactSales right join DimChannel on FactSales.channelKey=DimChannel.ChannelKey;
