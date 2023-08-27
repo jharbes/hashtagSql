b2f6f1e62cc4552afdc928e9f0a275217f7aced3
Jorge Harbes <jharbes@hotmail.com>
2023-08-27 10:52:11 -0300

    Create 04-chavePrimaria-vs-chaveEstrangeira.sql

---
+++ 008-sqlserver-joinsNoSql/04-chavePrimaria-vs-chaveEstrangeira.sql
@@ -0,0 +1,12 @@
+-- 04-chavePrimaria-vs-chaveEstrangeira
+
+-- Como relacionar tabelas?
+
+/*
+Uma chave primária é uma coluna que identifica as informacoes distintas de uma tabela.
+Geralmente é uma coluna de ID.
+
+Uma chave estrangeira é uma coluna que permite relacionar as linhas de uma segunda
+tabela com a chave primária de uma primeira tabela.
+*/
+
