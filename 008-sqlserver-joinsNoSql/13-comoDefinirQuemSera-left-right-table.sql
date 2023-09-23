cd75afbc2404a936dbd9c516edf6b340b4c40d0a
Jorge Harbes <jharbes@hotmail.com>
2023-09-23 15:08:55 -0300

    Update 13-comoDefinirQuemSera-left-right-table.sql

--- 008-sqlserver-joinsNoSql/13-comoDefinirQuemSera-left-right-table.sql
+++ 008-sqlserver-joinsNoSql/13-comoDefinirQuemSera-left-right-table.sql
@@ -2,6 +2,17 @@
 
 -- Como escolher quem é a tabela da esquerda e a tabela da direita?
 
+-- observe que se invertermos o join de left para right ou vice versa teremos
+-- como alteracao a tabela que retornara com TODOS as suas linhas, independente de
+-- terem relacao ou nao com a outra
+
+-- caso invertamos o join E as tabelas procuradas acabaremos tendo o mesmo resultado final
+
+-- no fim das contas o mais comum é utilizarmos o 'left join' pois assim retornaremos todos
+-- os valores da primeira tabela pesquisada e apenas complementaremos os valores associados
+-- da tabela a direita, caso esses nao existem retornarao como null, ou seja, o foco será 
+-- a tabela da esquerda
+
 select
 	id_produto,
 	nome_produto,
