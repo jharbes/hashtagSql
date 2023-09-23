3746e1fb5231676a5c77cf0cc760b783d35f1cf7
Jorge Harbes <jharbes@hotmail.com>
2023-09-23 12:05:52 -0300

    Update 10-fullJoin-ou-fullOuterJoin-explicacao.sql

--- 008-sqlserver-joinsNoSql/10-fullJoin-ou-fullOuterJoin-explicacao.sql
+++ 008-sqlserver-joinsNoSql/10-fullJoin-ou-fullOuterJoin-explicacao.sql
@@ -17,3 +17,18 @@ from
 	produtos
 full join
 	subcategoria on produtos.id_subcategoria=subcategoria.id_subcategoria;
+-- observe no exemplo acima que o 'id_subcategoria' de 'Câmera Digital' e 'Televisão'
+-- retornaram como nulos, mas isso so acontece porque o 'id_subcategoria' DESSA
+-- consulta é referente à tabela 'produtos' e não à tabela 'subcategoria', observe
+-- que na consulta abaixo o retorno desses ids já acontece
+
+
+select
+	id_produto,
+	nome_produto,
+	subcategoria.id_subcategoria,
+	subcategoria.nome_subcategoria
+from
+	produtos
+full join
+	subcategoria on produtos.id_subcategoria=subcategoria.id_subcategoria;
