a7cadd3f6b289474c89351cd74bf15028b8dcd5c
Jorge Harbes <jharbes@hotmail.com>
2023-09-23 14:35:12 -0300

    Update 12-inner-left-right-join-exemplos.sql

--- 008-sqlserver-joinsNoSql/12-inner-left-right-join-exemplos.sql
+++ 008-sqlserver-joinsNoSql/12-inner-left-right-join-exemplos.sql
@@ -15,3 +15,36 @@ inner join
 	DimProductSubcategory
 on
 	DimProduct.ProductSubcategoryKey=DimProductSubcategory.ProductSubcategoryKey;
+
+
+
+-- nesse caso o resultado do 'inner join' foi igual ao resultado do 'left join'
+-- ou seja, todos os produtos tem uma subcategoria associada
+select
+	ProductKey,
+	ProductName,
+	DimProduct.ProductSubcategoryKey,
+	ProductSubcategoryName
+from
+	DimProduct
+left join
+	DimProductSubcategory
+on
+	DimProduct.ProductSubcategoryKey=DimProductSubcategory.ProductSubcategoryKey;
+
+
+
+-- nesse caso o resultado é diferente do 'inner join' e do 'left join', pois existem
+-- subcategorias que não tem nenhum produto associado (no fim da consulta)
+-- (resultado maior que as ultimas duas consultas)
+select
+	ProductKey,
+	ProductName,
+	DimProduct.ProductSubcategoryKey,
+	ProductSubcategoryName
+from
+	DimProduct
+right join
+	DimProductSubcategory
+on
+	DimProduct.ProductSubcategoryKey=DimProductSubcategory.ProductSubcategoryKey;
