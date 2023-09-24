c0980bdeb4a7939e8c9c9a265c61af8836168a73
Jorge Harbes <jharbes@hotmail.com>
2023-09-24 12:14:40 -0300

    Update sqlserver-joinNoSql-exercicios-resolucao.sql

--- 008-sqlserver-joinsNoSql/sqlserver-joinNoSql-exercicios-resolucao.sql
+++ 008-sqlserver-joinsNoSql/sqlserver-joinNoSql-exercicios-resolucao.sql
@@ -225,3 +225,24 @@ from
 cross join DimChannel
 where
 	BrandName in ('Contoso','Fabrikam','Litware');
+
+
+
+
+/*
+9. Neste exercício, você deverá relacionar as tabelas FactOnlineSales com DimPromotion.
+Identifique a coluna que as duas tabelas têm em comum e utilize-a para criar esse
+relacionamento.
+
+Retorne uma tabela contendo as seguintes colunas:
+• OnlineSalesKey
+• DateKey
+• PromotionName
+• SalesAmount
+
+A sua consulta deve considerar apenas as linhas de vendas referentes a produtos com
+desconto (PromotionName <> ‘No Discount’). Além disso, você deverá ordenar essa tabela de
+acordo com a coluna DateKey, em ordem crescente.
+
+*/
+
