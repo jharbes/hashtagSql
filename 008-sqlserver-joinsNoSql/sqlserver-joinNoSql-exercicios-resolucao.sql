6e4fafd043b2bc2afc2e2981a736536985fab6ff
Jorge Harbes <jharbes@hotmail.com>
2023-09-24 10:57:19 -0300

    Update sqlserver-joinNoSql-exercicios-resolucao.sql

--- 008-sqlserver-joinsNoSql/sqlserver-joinNoSql-exercicios-resolucao.sql
+++ 008-sqlserver-joinsNoSql/sqlserver-joinNoSql-exercicios-resolucao.sql
@@ -76,3 +76,29 @@ from
 	DimStore
 left join DimGeography
 	on DimStore.GeographyKey=DimGeography.GeographyKey;
+
+
+
+
+/*
+4. Complementa a tabela DimProduct com a informação de ProductCategoryDescription. Utilize
+o LEFT JOIN e retorne em seu SELECT apenas as 5 colunas que considerar mais relevantes
+
+*/
+
+select * from DimProduct;
+select * from DimProductCategory;
+select * from DimProductSubcategory;
+
+select
+	ProductKey,
+	ProductName,
+	ProductDescription,
+	ProductSubcategoryName,
+	ProductCategoryName
+from
+	DimProduct
+left join DimProductSubcategory
+	on DimProduct.ProductSubcategoryKey=DimProductSubcategory.ProductSubcategoryKey
+left join DimProductCategory
+	on DimProductSubcategory.ProductCategoryKey=DimProductCategory.ProductCategoryKey;
