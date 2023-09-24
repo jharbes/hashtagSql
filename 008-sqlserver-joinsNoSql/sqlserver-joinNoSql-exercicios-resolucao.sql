9687cac4e4943b4935ddc5ad5d0e00cb9bfc8579
Jorge Harbes <jharbes@hotmail.com>
2023-09-24 09:34:11 -0300

    Create sqlserver-joinNoSql-exercicios-resolucao.sql

---
+++ 008-sqlserver-joinsNoSql/sqlserver-joinNoSql-exercicios-resolucao.sql
@@ -0,0 +1,17 @@
+-- Exercicios - joinsNoSql
+
+/*
+Os Joins têm como principal objetivo deixar as tabelas mais informativas, permitindo buscar
+dados de uma tabela para outra, de acordo com uma coluna em comum que permita essa
+relação.
+Dito isso, faça os seguintes exercícios:
+*/
+
+use ContosoRetailDW;
+
+/*
+1. Utilize o INNER JOIN para trazer os nomes das subcategorias dos produtos, da tabela
+DimProductSubcategory para a tabela DimProduct.
+
+*/
+
