68360cf4560bbbecd9063e97268efd950c6a5f08
Jorge Harbes <jharbes@hotmail.com>
2023-10-01 14:25:23 -0300

    Update sqlserver-groupBy_mais_joins-listaExercicios-resolucao.sql

--- 009-groupBy-mais-joins-aplicacoes/sqlserver-groupBy_mais_joins-listaExercicios-resolucao.sql
+++ 009-groupBy-mais-joins-aplicacoes/sqlserver-groupBy_mais_joins-listaExercicios-resolucao.sql
@@ -16,4 +16,4 @@ quantidade total devolvida (Return Quantity) de acordo com o nome das lojas
 (StoreName).
 
 c) Faça um resumo do valor total vendido (Sales Amount) para cada mês
-(CalendarMonthLabel) e ano (CalendarYear).*/-- a)
+(CalendarMonthLabel) e ano (CalendarYear).*/-- a)select top(10) * from factsales;select * from DimChannel;select	ChannelName as 'Canal de Vendas',	sum(SalesQuantity) as 'Quantidade Total Vendida'from	FactSalesinner join DimChannel	on FactSales.channelKey=DimChannel.ChannelKeygroup by	ChannelNameorder by	sum(SalesQuantity) desc;-- b)select	StoreName as 'Loja',	sum(SalesQuantity) as 'Quantidade Total Vendida',	sum(ReturnQuantity) as 'Quantidade Total Devolvida'from	FactSalesinner join DimStore	on FactSales.StoreKey=DimStore.StoreKeygroup by	StoreName;-- c)select	CalendarMonthLabel as 'Mês',	CalendarYear as 'Ano',	sum(SalesAmount) as 'Valor Total Vendido'from	FactSalesinner join DimDate	on FactSales.DateKey=DimDate.Datekeygroup by	CalendarYear, FiscalMonth, CalendarMonthLabelorder by	CalendarYear, FiscalMonth;
