e5e29d43bf657f92886ad68d2f7f283257327af9
Jorge Harbes <jharbes@hotmail.com>
2023-10-08 13:46:05 -0300

    Create sqlserver-variaveis-exercicios-solucao.sql

---
+++ 010-sqlserver-variaveis/sqlserver-variaveis-exercicios-solucao.sql
@@ -0,0 +1,31 @@
+-- exercicios - variaveis - aplicacoes
+
+use ContosoRetailDW;
+
+
+-- MÓDULO 10: EXERCÍCIOS
+
+/*
+1. Declare 4 variáveis inteiras. Atribua os seguintes valores a elas:
+
+valor1 = 10
+valor2 = 5
+valor3 = 34
+valor4 = 7
+
+a) Crie uma nova variável para armazenar o resultado da soma entre valor1 e valor2. Chame
+essa variável de soma.
+
+b) Crie uma nova variável para armazenar o resultado da subtração entre valor3 e valor 4.
+Chame essa variável de subtracao.
+
+c) Crie uma nova variável para armazenar o resultado da multiplicação entre o valor 1 e o
+valor4. Chame essa variável de multiplicacao.
+
+d) Crie uma nova variável para armazenar o resultado da divisão do valor3 pelo valor4. Chame
+essa variável de divisao. Obs: O resultado deverá estar em decimal, e não em inteiro.
+
+e) Arredonde o resultado da letra d) para 2 casas decimais.
+
+*/
+
