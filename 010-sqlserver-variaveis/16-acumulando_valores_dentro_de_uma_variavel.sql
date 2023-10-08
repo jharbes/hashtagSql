86e17d3328a689be51802630ee76b0791ae13846
Jorge Harbes <jharbes@hotmail.com>
2023-10-08 13:34:12 -0300

    Update 16-acumulando_valores_dentro_de_uma_variavel.sql

--- 010-sqlserver-variaveis/16-acumulando_valores_dentro_de_uma_variavel.sql
+++ 010-sqlserver-variaveis/16-acumulando_valores_dentro_de_uma_variavel.sql
@@ -2,15 +2,45 @@
 -- Aula 16 de 23: Acumulando valores dentro de uma variável
 
 -- Exemplo 1: Retorne uma lista com os nomes dos funcionários do departamento de Marketing
+
+select
+	FirstName + ' ' + LastName as 'Nome Completo'
+from
+	DimEmployee
+where
+	DepartmentName='Marketing'
+
+
 DECLARE @ListaNomes VARCHAR(MAX)
 SET @ListaNomes = ''
 
+SELECT
+	@ListaNomes = @ListaNomes + FirstName + ', ' -- + CHAR(10)
+FROM
+	DimEmployee
+WHERE DepartmentName = 'Marketing'
+
+
+print @listanomes
+
+-- utilizamos o DATALENGTH para retirar a virgula(,) do final
+PRINT LEFT(@ListaNomes, DATALENGTH(@ListaNomes) - 2)
+
+
+-----------------------------------------------------------------
+
+
+SET @ListaNomes = ''
+
+-- o valor CHAR(10) está sendo utilizado para sinalizar um 'pulo de linha'
 SELECT
 	@ListaNomes = @ListaNomes + FirstName + ', ' + CHAR(10)
 FROM
 	DimEmployee
 WHERE DepartmentName = 'Marketing'
 
+
+print @listanomes
+
+-- utilizamos o DATALENGTH para retirar a virgula(,) do final
 PRINT LEFT(@ListaNomes, DATALENGTH(@ListaNomes) - 3)
-
-
