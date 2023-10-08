a29c5c7e3963d0cf25789a30220d01a2ebc7861e
Jorge Harbes <jharbes@hotmail.com>
2023-10-08 11:28:49 -0300

    Update 12-utilizando_uma_variavel_em_uma_consulta-parte2.sql

--- 010-sqlserver-variaveis/12-utilizando_uma_variavel_em_uma_consulta-parte2.sql
+++ 010-sqlserver-variaveis/12-utilizando_uma_variavel_em_uma_consulta-parte2.sql
@@ -26,3 +26,58 @@ FROM
 	DimEmployee
 WHERE BirthDate >= @varData
 ORDER BY Nascimento
+
+
+
+-- Exemplo2:
+-- Descobrindo os aniversariantes
+
+/*
+create function calcular_idade(@BirthDate datetime)
+returns int
+as
+begin
+	declare @idade int;
+
+	set @idade=year(CURRENT_TIMESTAMP)-year(@BirthDate)
+
+	IF (MONTH(@BirthDate) > MONTH(@BirthDate)) OR 
+       (MONTH(@BirthDate) = MONTH(@BirthDate) AND DAY(@BirthDate) > DAY(@BirthDate))
+    BEGIN
+        SET @idade = @idade - 1
+    END
+
+    RETURN @idade
+end
+
+*/
+
+DECLARE @dia_aniversario int
+declare @mes_aniversario int
+
+set @dia_aniversario=day(CURRENT_TIMESTAMP)
+set @mes_aniversario=MONTH(CURRENT_TIMESTAMP)
+
+
+SELECT 
+	FirstName AS 'Nome',
+	LastName AS 'Sobrenome',
+	BirthDate AS 'Nascimento',
+	--calcular_idade(BirthDate) as 'Idade',
+	'Cliente' AS 'Tipo'
+FROM	
+	DimCustomer
+WHERE day(BirthDate) = @dia_aniversario and MONTH(BirthDate)=@mes_aniversario
+
+UNION
+
+SELECT 
+	FirstName AS 'Nome',
+	LastName AS 'Sobrenome',
+	BirthDate AS 'Nascimento',
+	--year(CURRENT_TIMESTAMP)-year(BirthDate) as 'Idade',
+	'Funcionário' AS 'Tipo'
+FROM	
+	DimEmployee
+WHERE day(BirthDate) = @dia_aniversario and MONTH(BirthDate)=@mes_aniversario
+ORDER BY 'Nome'
