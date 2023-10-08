59d79962845ab826f4d96442f9b124ad74186ac6
Jorge Harbes <jharbes@hotmail.com>
2023-10-08 14:17:08 -0300

    Update sqlserver-variaveis-exercicios-solucao.sql

--- 010-sqlserver-variaveis/sqlserver-variaveis-exercicios-solucao.sql
+++ 010-sqlserver-variaveis/sqlserver-variaveis-exercicios-solucao.sql
@@ -34,6 +34,8 @@ declare @valor1 int = 10, @valor2 int = 5, @valor3 int = 34, @valor4 int = 7
 -- a)
 declare @soma int
 select @soma=@valor1+@valor2
+-- ou
+-- set @soma=@valor1+@valor2
 select @soma
 print @soma
 
@@ -56,3 +58,21 @@ print @divisao
 -- e)
 
 select ROUND(@divisao,2)
+print round(@divisao,2)
+
+
+
+/*
+2. Para cada declaração das variáveis abaixo, atenção em relação ao tipo de dado que deverá ser
+especificado.
+
+a) Declare uma variável chamada ‘produto’ e atribua o valor de ‘Celular’.
+b) Declare uma variável chamada ‘quantidade’ e atribua o valor de 12.
+c) Declare uma variável chamada ‘preco’ e atribua o valor 9.99.
+d) Declare uma variável chamada ‘faturamento’ e atribua o resultado da multiplicação entre
+‘quantidade’ e ‘preco’.
+e) Visualize o resultado dessas 4 variáveis em uma única consulta, por meio do SELECT.
+
+*/
+
+-- a)
