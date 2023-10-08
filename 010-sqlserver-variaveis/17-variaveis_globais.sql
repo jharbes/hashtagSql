72ac14abeaed30dbf20ad6a3696cbb1df0ac9f00
Jorge Harbes <jharbes@hotmail.com>
2023-10-08 13:42:00 -0300

    Update 17-variaveis_globais.sql

--- 010-sqlserver-variaveis/17-variaveis_globais.sql
+++ 010-sqlserver-variaveis/17-variaveis_globais.sql
@@ -1,11 +1,17 @@
 -- [SQL Server] Variáveis
 -- Aula 17 de 23: Variáveis Globais
 
+-- Variaveis globais são variaveis que já existem primitivamente no SQL Server
+-- Elas utilizam duas arrobas (@@) na frente do seu nome em vez de uma
 
+
+-- @@SERVERNAME retorna o nome do servidor
 SELECT @@SERVERNAME
 
+-- @@VERSION retorna a versão do SGBD
 SELECT @@VERSION
 
-
 SELECT * FROM DimProduct
+
+-- @@ROWCOUNT retorna o numero de linhas da ultima tabela consultada (nesse caso DimProduct)
 SELECT @@ROWCOUNT
