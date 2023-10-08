63f41dac1bd9a428975358f19b5dcc03a90deeca
Jorge Harbes <jharbes@hotmail.com>
2023-10-08 14:24:07 -0300

    Update sqlserver-variaveis-exercicios-solucao.sql

--- 010-sqlserver-variaveis/sqlserver-variaveis-exercicios-solucao.sql
+++ 010-sqlserver-variaveis/sqlserver-variaveis-exercicios-solucao.sql
@@ -93,3 +93,23 @@ select @faturamento
 
 -- e)
 select @produto as 'Produto', @quantidade as 'Quantidade', @preco as 'Preço', @faturamento as 'Faturamento'
+
+
+
+/*
+3. Você é responsável por gerenciar um banco de dados onde são recebidos dados externos de
+usuários. Em resumo, esses dados são:
+- Nome do usuário
+- Data de nascimento
+- Quantidade de pets que aquele usuário possui
+
+Você precisará criar um código em SQL capaz de juntar as informações fornecidas por este
+usuário. Para simular estes dados, crie 3 variáveis, chamadas: nome, data_nascimento e
+num_pets. Você deverá armazenar os valores ‘André’, ‘10/02/1998’ e 2, respectivamente.
+
+O resultado final a ser alcançado é mostrado no print abaixo:
+
+Dica: você precisará utilizar as funções CAST e FORMAT para chegar no resultado.
+
+*/
+
