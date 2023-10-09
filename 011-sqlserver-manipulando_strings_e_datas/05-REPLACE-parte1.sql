7db21ba8a528c2c06e3bdff30456244f1aa280ec
Jorge Harbes <jharbes@hotmail.com>
2023-10-09 09:39:58 -0300

    Update 05-REPLACE-parte1.sql

--- 011-sqlserver-manipulando_strings_e_datas/05-REPLACE-parte1.sql
+++ 011-sqlserver-manipulando_strings_e_datas/05-REPLACE-parte1.sql
@@ -3,12 +3,27 @@
 
 -- REPLACE ---> Substitui um determinado texto por outro texto
 
+select 'O Excel é o melhor'; -- O Excel é o melhor
+
+select REPLACE('O Excel é o melhor', 'Excel', 'SQL'); -- O SQL é o melhor
+
+
 -- Exemplo: Crie uma consulta a partir de DimCustomer onde você retorna o Nome Completo dos Clientes, a coluna de Sexo (Abrev) e uma outra coluna de Sexo substituindo M por Masculino e F por Feminino
 
-SELECT * FROM DimCustomer
+SELECT * FROM DimCustomer;
+
+SELECT
+	CONCAT(FirstName, ' ', LastName) AS 'Nome Completo',
+	Gender AS 'Sexo (Abrev)',
+	REPLACE(Gender, 'M', 'Masculino') AS 'Sexo (Extenso)'
+FROM
+	DimCustomer;
+
+
+
 SELECT
 	CONCAT(FirstName, ' ', LastName) AS 'Nome Completo',
 	Gender AS 'Sexo (Abrev)',
 	REPLACE(REPLACE(Gender, 'M', 'Masculino'), 'F', 'Feminino') AS 'Sexo (Extenso)'
 FROM
-	DimCustomer
+	DimCustomer;
