c8c92e785a62d9895d17e6462fa709ee4e3ff49e
Jorge Harbes <jharbes@hotmail.com>
2023-10-09 09:48:49 -0300

    Update 05-REPLACE-parte1.sql

--- 011-sqlserver-manipulando_strings_e_datas/05-REPLACE-parte1.sql
+++ 011-sqlserver-manipulando_strings_e_datas/05-REPLACE-parte1.sql
@@ -1,11 +1,13 @@
 -- [SQL Server] Manipulando Strings e Datas no SQL
 -- Aula 5 e 6 de 23: REPLACE (Parte 1 e 2)
 
--- REPLACE ---> Substitui um determinado texto por outro texto
+-- REPLACE ---> Substitui um determinado texto por outro texto, NÃO é CASE SENSITIVE
 
 select 'O Excel é o melhor'; -- O Excel é o melhor
 
 select REPLACE('O Excel é o melhor', 'Excel', 'SQL'); -- O SQL é o melhor
+
+select REPLACE('O Excel é o melhor', 'EXCEL', 'SQL'); -- O SQL é o melhor
 
 
 -- Exemplo: Crie uma consulta a partir de DimCustomer onde você retorna o Nome Completo dos Clientes, a coluna de Sexo (Abrev) e uma outra coluna de Sexo substituindo M por Masculino e F por Feminino
@@ -21,9 +23,16 @@ FROM
 
 
 
+-- observe que nesse caso fazemos o PRIMEIRO replace onde 'M' irá virar 'Masculino'
+-- o SEGUNDO replace transformará 'F' em 'Feminino (caso exista) gerando a possibilidade
+-- de dois replaces e contemplando as duas mudanças desejadas no Gender
 SELECT
 	CONCAT(FirstName, ' ', LastName) AS 'Nome Completo',
 	Gender AS 'Sexo (Abrev)',
 	REPLACE(REPLACE(Gender, 'M', 'Masculino'), 'F', 'Feminino') AS 'Sexo (Extenso)'
 FROM
 	DimCustomer;
+
+-- observe que devemos ter cuidado na ordem dos replaces e nem sempre replaces seguidos darão
+-- certo pois algumas letras poderao se repetir apos o replace sendo alteradas continuamente
+-- observacao que tambem o REPLACE NÃO é case sensitive
