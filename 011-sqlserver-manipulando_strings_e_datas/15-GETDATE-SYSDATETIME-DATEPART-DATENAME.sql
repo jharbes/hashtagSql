93e680943eb74dbd715a6d31abd7fb116d916283
Jorge Harbes <jharbes@hotmail.com>
2023-10-09 15:35:11 -0300

    Update 15-GETDATE-SYSDATETIME-DATEPART-DATENAME.sql

--- 011-sqlserver-manipulando_strings_e_datas/15-GETDATE-SYSDATETIME-DATEPART-DATENAME.sql
+++ 011-sqlserver-manipulando_strings_e_datas/15-GETDATE-SYSDATETIME-DATEPART-DATENAME.sql
@@ -5,29 +5,37 @@
 -- SYSDATETIME: Retorna a data/hora atual do sistema (mais preciso que a GETDATE)
 -- DATENAME e DATEPART: Retornam informações (dia, mês, ano, semana, etc) de uma data
 
-SELECT GETDATE()
-SELECT SYSDATETIME()
+SELECT GETDATE(); -- 2023-10-09 15:23:50.390
+SELECT SYSDATETIME(); -- 2023-10-09 15:23:50.3942775
 
--- DATENAME: Retorna o resultado em formato de TEXTO
 
-DECLARE @varData DATETIME
-SET @varData = GETDATE()
-SELECT
-	DATENAME(DAY, @varData) AS 'Dia',
-	DATENAME(MONTH, @varData) AS 'Mês',
-	DATENAME(YEAR, @varData) AS 'Ano',
-	DATENAME(DAYOFYEAR, @varData) AS 'Dia do ano'
 
--- DATEPART: Retorna o resultado em formato de NÚMERO
+-- DATENAME: Retorna o resultado em formato de TEXTO (nvarchar)
 
-DECLARE @varData DATETIME
-SET @varData = GETDATE()
-SELECT
-	DATEPART(DAY, @varData) AS 'Dia',
-	DATEPART(MONTH, @varData) AS 'Mês',
-	DATEPART(YEAR, @varData) AS 'Ano',
-	DATEPART(DAYOFYEAR, @varData) AS 'Dia do ano'
+DECLARE @varData DATETIME;
+SET @varData = GETDATE();
 
 SELECT
-	SQL_VARIANT_PROPERTY(DATENAME(DAY, @varData), 'BaseType'),
-	SQL_VARIANT_PROPERTY(DATEPART(DAY, @varData), 'BaseType')
+	DATENAME(DAY, @varData) AS 'Dia', -- 9
+	DATENAME(MONTH, @varData) AS 'Mês', -- Outubro
+	DATENAME(YEAR, @varData) AS 'Ano', -- 2023
+	DATENAME(DAYOFYEAR, @varData) AS 'Dia do ano'; -- 282
+
+
+
+-- DATEPART: Retorna o resultado em formato de NÚMERO (int)
+
+DECLARE @varData1 DATETIME;
+SET @varData1 = GETDATE();
+
+SELECT
+	DATEPART(DAY, @varData1) AS 'Dia', -- 9
+	DATEPART(MONTH, @varData1) AS 'Mês', -- 10
+	DATEPART(YEAR, @varData1) AS 'Ano', -- 2023
+	DATEPART(DAYOFYEAR, @varData1) AS 'Dia do ano'; -- 282
+
+SELECT
+	SQL_VARIANT_PROPERTY(DATENAME(DAY, @varData1), 'BaseType'), -- nvarchar
+	SQL_VARIANT_PROPERTY(DATEPART(DAY, @varData1), 'BaseType'), -- int
+	SQL_VARIANT_PROPERTY(DATENAME(DAYOFYEAR, @varData1), 'BaseType'), -- nvarchar
+	SQL_VARIANT_PROPERTY(DATEPART(DAYOFYEAR, @varData1), 'BaseType'); -- int
