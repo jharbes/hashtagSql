1a5f6206600760afe9f0d5537ecb2a9b5f7d9f5b
Jorge Harbes <jharbes@hotmail.com>
2023-10-09 09:28:15 -0300

    Update 04-LEFT-RIGHT.sql

--- 011-sqlserver-manipulando_strings_e_datas/04-LEFT-RIGHT.sql
+++ 011-sqlserver-manipulando_strings_e_datas/04-LEFT-RIGHT.sql
@@ -2,14 +2,25 @@
 -- Aula 4 de 23: LEFT e RIGHT
 
 -- LEFT ---> Extrai uma determinada quantidade de caracteres de um texto, da esquerda para a direita
+
+select LEFT('12345678', 4); -- 1234
+
+
 -- RIGHT ---> Extrai uma determinada quantidade de caracteres de um texto, da direita para a esquerda
+
+select RIGHT('12345678', 4); -- 5678
+
+
 -- Exemplo: Faça uma consulta à tabela DimProduct e divida a coluna de StyleName em 2 partes
 
-SELECT * FROM DimProduct
+SELECT * FROM DimProduct;
+
 SELECT
+	ProductKey as 'ID do Produto',
 	ProductName AS 'Produto',
 	UnitPrice AS 'Preço',
+	StyleName,
 	LEFT(StyleName, 7) AS 'Cod 1',
 	RIGHT(StyleName, 7) AS 'Cod 2'
 FROM
-	DimProduct
+	DimProduct;
