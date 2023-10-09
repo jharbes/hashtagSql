cbacd6d89624bf7474213555b402e28dfb1c6b7e
Jorge Harbes <jharbes@hotmail.com>
2023-10-09 12:21:05 -0300

    Update 10-CHARINDEX-SUBSTRING-parte1.sql

--- 011-sqlserver-manipulando_strings_e_datas/10-CHARINDEX-SUBSTRING-parte1.sql
+++ 011-sqlserver-manipulando_strings_e_datas/10-CHARINDEX-SUBSTRING-parte1.sql
@@ -4,13 +4,23 @@
 -- CHARINDEX: Descobre a posição de um determinado caractere dentro de um texto
 -- SUBSTRING: Extrai alguns caracteres de dentro de um texto
 
-SELECT CHARINDEX('Moreno', 'Raquel Moreno') AS 'Posição Sobrenome'
+-- mostra em que caractere se inicia a string do primeiro argumento em relaçao a string
+-- total (segundo argumento)
+SELECT CHARINDEX('Moreno', 'Raquel Moreno') AS 'Posição Sobrenome' -- 8
 
-SELECT SUBSTRING('Raquel Moreno', 8, 6) AS 'Sobrenome'
+
+-- na ausencia da posição procurada a função retornará 0 (zero)
+select CHARINDEX('X', 'Raquel Moreno') -- 0
+
+
+-- inicia o recorte da String no valor do segundo argumento pelo numero de caracteres definido
+-- no terceiro argumento
+SELECT SUBSTRING('Raquel Moreno', 8, 6) AS 'Sobrenome' -- Moreno
 
 
 SELECT 'Marcus Cavalcanti' AS 'Nome'
 
-SELECT CHARINDEX(' ', 'Marcus Cavalcanti') AS 'Posição'
+SELECT CHARINDEX(' ', 'Marcus Cavalcanti') AS 'Posição' -- 7
 
-SELECT SUBSTRING('Marcus Cavalcanti', CHARINDEX(' ', 'Marcus Cavalcanti') + 1, 100) AS 'Sobrenome'
+-- utilizando para extrair o sobrenome (nome que está após o espaço)
+SELECT SUBSTRING('Marcus Cavalcanti', CHARINDEX(' ', 'Marcus Cavalcanti') + 1, 100) AS 'Sobrenome' -- Cavalcanti
