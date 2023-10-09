4a52eb33b79d8f16f62a3a6c71397af6369bd68f
Jorge Harbes <jharbes@hotmail.com>
2023-10-09 10:30:56 -0300

    Update 08-UPPER-LOWER.sql

--- 011-sqlserver-manipulando_strings_e_datas/08-UPPER-LOWER.sql
+++ 011-sqlserver-manipulando_strings_e_datas/08-UPPER-LOWER.sql
@@ -5,10 +5,11 @@
 -- LOWER ---> Transforma um texto em minúscula
 -- Exemplo: Faça uma consulta à tabela DimCustomer e utilize as funções UPPER e LOWER na coluna de FirstName para observar o resultado
 
-SELECT * FROM DimCustomer
+SELECT * FROM DimCustomer;
+
 SELECT
 	UPPER(FirstName) AS 'NOME',
 	LOWER(FirstName) AS 'nome',
 	EmailAddress AS 'E-mail'
 FROM
-	DimCustomer
+	DimCustomer;
