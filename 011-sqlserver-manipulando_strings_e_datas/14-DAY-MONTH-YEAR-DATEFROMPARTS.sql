b4f08ebab6bf1d34888eed39f5253fd4768dd676
Jorge Harbes <jharbes@hotmail.com>
2023-10-09 15:19:20 -0300

    Update 14-DAY-MONTH-YEAR-DATEFROMPARTS.sql

--- 011-sqlserver-manipulando_strings_e_datas/14-DAY-MONTH-YEAR-DATEFROMPARTS.sql
+++ 011-sqlserver-manipulando_strings_e_datas/14-DAY-MONTH-YEAR-DATEFROMPARTS.sql
@@ -7,9 +7,13 @@ DECLARE @varData DATETIME
 SET @varData = '18/05/2020'
 
 SELECT
-	DAY(@varData) AS 'Dia',
-	MONTH(@varData) AS 'Mês',
-	YEAR(@varData) AS 'Ano'
+	SQL_VARIANT_PROPERTY(@varData, 'BaseType') as 'Tipo de @varData', -- datetime
+	DAY(@varData) AS 'Dia', -- 18
+	SQL_VARIANT_PROPERTY(DAY(@varData), 'BaseType') as 'Tipo de DAY()', -- int
+	MONTH(@varData) AS 'Mês', -- 5
+	SQL_VARIANT_PROPERTY(MONTH(@varData), 'BaseType') as 'Tipo de MONTH()', -- int
+	YEAR(@varData) AS 'Ano', -- 2020
+	SQL_VARIANT_PROPERTY(YEAR(@varData), 'BaseType') as 'Tipo de YEAR()'; -- int
 
 
 -- Utilize a função DATEFROMPARTS para obter uma data a partir das informações de dia, mês e ano
@@ -20,4 +24,5 @@ SET @varMes = 12
 SET @varAno = 2020
 
 SELECT
-	DATEFROMPARTS(@varAno, @varMes, @varDia) AS 'Data'
+	DATEFROMPARTS(@varAno, @varMes, @varDia) AS 'Data', -- 2020-12-29
+	SQL_VARIANT_PROPERTY(DATEFROMPARTS(@varAno, @varMes, @varDia), 'BaseType') as 'Tipo da Data'; -- date
