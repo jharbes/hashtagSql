b42ca8dea2d13ca1fcb7234394b7b3c2487efc25
Jorge Harbes <jharbes@hotmail.com>
2023-10-10 07:27:55 -0300

    Update sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql

--- 011-sqlserver-manipulando_strings_e_datas/sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql
+++ 011-sqlserver-manipulando_strings_e_datas/sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql
@@ -85,3 +85,13 @@ select
 	REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(StyleName, '0', 'A'), '1', 'B'), '2', 'C'), '3', 'D'), '4', 'E'), '5', 'F'), '6', 'G'), '7', 'H'), '8', 'I'), '9', 'J') as 'StyleName Alterado'
 from
 	DimProduct;
+
+-- ou
+
+select
+	ProductKey,
+	ProductName,
+	StyleName,
+	TRANSLATE(StyleName, '0123456789', 'ABCDEFGHIJ') as 'StyleName Alterado'
+from
+	DimProduct;
