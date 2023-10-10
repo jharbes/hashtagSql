6f523cf8e3c963a5f3c1105e20c4a407d0d15a6c
Jorge Harbes <jharbes@hotmail.com>
2023-10-10 07:32:42 -0300

    Update sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql

--- 011-sqlserver-manipulando_strings_e_datas/sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql
+++ 011-sqlserver-manipulando_strings_e_datas/sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql
@@ -95,3 +95,25 @@ select
 	TRANSLATE(StyleName, '0123456789', 'ABCDEFGHIJ') as 'StyleName Alterado'
 from
 	DimProduct;
+
+
+
+/*
+3. O setor de TI está criando um sistema para acompanhamento individual de cada funcionário da
+empresa Contoso. Cada funcionário receberá um login e senha. O login de cada funcionário será
+o ID do e-mail, como no exemplo abaixo:
+
+Já a senha será o FirtName + o dia do ano em que o funcionário nasceu, em MAIÚSCULA. Por
+exemplo, o funcionário com E-mail: mark0@contoso.com e data de nascimento 15/01/1990
+deverá ter a seguinte senha:
+
+Login: mark0
+Senha: MARK15
+
+O responsável pelo TI pediu a sua ajuda para retornar uma tabela contendo as seguintes colunas
+da tabela DimEmployee: Nome completo (FirstName + LastName), E-mail, ID do e-mail e Senha.
+
+Portanto, faça uma consulta à tabela DimProduct e retorne esse resultado
+
+*/
+
