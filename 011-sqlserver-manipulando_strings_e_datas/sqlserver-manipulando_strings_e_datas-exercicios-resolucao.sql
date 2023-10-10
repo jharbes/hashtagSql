aa49451936f264d7f37103a8f0b99e2e9685a2c8
Jorge Harbes <jharbes@hotmail.com>
2023-10-10 09:18:27 -0300

    Update sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql

--- 011-sqlserver-manipulando_strings_e_datas/sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql
+++ 011-sqlserver-manipulando_strings_e_datas/sqlserver-manipulando_strings_e_datas-exercicios-resolucao.sql
@@ -164,3 +164,34 @@ select
 from
 	DimCustomer
 where YEAR(DateFirstPurchase)=2001;
+
+
+
+/*
+5. A tabela DimEmployee possui uma informação de data de contratação (HireDate). A área de
+RH, no entanto, precisa das informações dessas datas de forma separada em dia, mês e ano, pois
+será feita uma automatização para criação de um relatório de RH, e facilitaria muito se essas
+informações estivessem separadas em uma tabela.
+
+Você deverá realizar uma consulta à tabela DimEmployee e retornar uma tabela contendo as
+seguintes informações: FirstName, EmailAddress, HireDate, além das colunas de Dia, Mês e Ano
+de contratação.
+
+Obs1: A coluna de Mês deve conter o nome do mês por extenso, e não o número do mês.
+
+Obs2: Lembre-se de nomear cada uma dessas colunas em sua consulta para garantir que o
+entendimento de cada informação ficará 100% claro
+
+*/
+
+select * from DimEmployee;
+
+select
+	FirstName as 'Primeiro Nome',
+	EmailAddress as 'E-Mail',
+	HireDate as 'Data da Contratação',
+	DAY(HireDate) as 'Dia da Contratação',
+	DATENAME(MONTH, HireDate) as 'Mês da Contratação',
+	YEAR(HireDate) as 'Ano da Contratação'
+from
+	DimEmployee;
