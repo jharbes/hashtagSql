af3d9f175a5088122a228c740de6e8093170f076
Jorge Harbes <jharbes@hotmail.com>
2023-11-02 16:39:26 -0300

    Update 11-ISNULL-tratando_valores_nulos.sql

--- 012-sqlserver-funcoes_condicionais/11-ISNULL-tratando_valores_nulos.sql
+++ 012-sqlserver-funcoes_condicionais/11-ISNULL-tratando_valores_nulos.sql
@@ -1,7 +1,17 @@
 -- [SQL Server] Funções Condicionais
 -- Aula 11 de 18: ISNULL - Tratando valores nulos
 
+
+-- A função ISNULL servirá para substituir os valores que estejam como NULOS por outro de sua escolha
+
+-- Funcionamento
+-- ISNULL(<parametro_ou_coluna>,<valor_desejado_para_substituicao>)
+
 -- Exemplo: Faça uma consulta que substitua todos os valores nulos de CityName da tabela DimGeography pelo texto 'Local desconhecido'.
+
+
+select * from DimGeography;
+
 
 SELECT
 	GeographyKey,
@@ -9,4 +19,4 @@ SELECT
 	CityName,
 	ISNULL(CityName, 'Local desconhecido')
 FROM
-	DimGeography
+	DimGeography;
