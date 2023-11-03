af0dd90954dff39fcc4fe50af7153ff2bdf25c66
Jorge Harbes <jharbes@hotmail.com>
2023-11-03 10:10:03 -0300

    Update sqlserver-funcoes_condicionais-exercicios-resolucao.sql

--- 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
+++ 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
@@ -25,6 +25,8 @@ declare @desconto_deluxe float = 0.09;
 select
 	p.ProductKey,
 	p.ProductName,
+	p.ClassName,
+	p.UnitPrice,
 	p.Desconto,
 	p.UnitPrice * (1 - p.Desconto) as 'Preço com Desconto'
 from (
@@ -32,6 +34,7 @@ from (
 		ProductKey,
 		ProductName,
 		UnitPrice,
+		ClassName,
 		case
 			when ClassName='Economy' then @desconto_economy
 			when ClassName='Regular' then @desconto_regular
@@ -40,3 +43,30 @@ from (
 		end as Desconto
 	from DimProduct
 ) as p;
+
+
+-- ou
+
+
+declare @desconto_economy1 float = 0.05;
+declare @desconto_regular1 float = 0.07;
+declare @desconto_deluxe1 float = 0.09;
+
+select
+	ProductKey,
+	ProductName,
+	UnitPrice,
+	ClassName,
+	case
+		when ClassName='Economy' then @desconto_economy1
+		when ClassName='Regular' then @desconto_regular1
+		when ClassName='Deluxe' then @desconto_deluxe1
+		else 0.00
+	end as Desconto,
+	case
+		when ClassName='Economy' then UnitPrice * (1 - @desconto_economy1)
+		when ClassName='Regular' then UnitPrice * (1 - @desconto_regular1)
+		when ClassName='Deluxe' then UnitPrice * (1 - @desconto_deluxe1)
+		else UnitPrice
+	end as 'Preço com Desconto'
+from DimProduct;
