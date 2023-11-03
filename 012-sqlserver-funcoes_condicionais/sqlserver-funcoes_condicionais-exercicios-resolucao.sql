f2bb2dd9f1621e6b95b86e0515adc50446baa256
Jorge Harbes <jharbes@hotmail.com>
2023-11-03 09:19:50 -0300

    Create sqlserver-funcoes_condicionais-exercicios-resolucao.sql

---
+++ 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
@@ -0,0 +1,21 @@
+-- exercicios - funcoes_condicionais
+
+use ContosoRetailDW;
+
+
+-- MÓDULO 12: EXERCÍCIOS
+
+/*
+1. O setor de vendas decidiu aplicar um desconto aos produtos de acordo com a sua classe. O
+percentual aplicado deverá ser de:
+
+Economy -> 5%
+Regular -> 7%
+Deluxe -> 9%
+
+a) Faça uma consulta à tabela DimProduct que retorne as seguintes colunas: ProductKey,
+ProductName, e outras duas colunas que deverão retornar o % de Desconto e UnitPrice com
+desconto.
+
+b) Faça uma adaptação no código para que os % de desconto de 5%, 7% e 9% sejam facilmente
+modificados (dica: utilize variáveis).*/
