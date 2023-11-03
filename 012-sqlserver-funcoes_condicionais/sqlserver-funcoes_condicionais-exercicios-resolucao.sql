75fe6b51097a10608ba772efec228280b6059967
Jorge Harbes <jharbes@hotmail.com>
2023-11-03 09:34:41 -0300

    Update sqlserver-funcoes_condicionais-exercicios-resolucao.sql

--- 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
+++ 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
@@ -18,4 +18,25 @@ ProductName, e outras duas colunas que deverão retornar o % de Desconto e UnitPr
 desconto.
 
 b) Faça uma adaptação no código para que os % de desconto de 5%, 7% e 9% sejam facilmente
-modificados (dica: utilize variáveis).*/
+modificados (dica: utilize variáveis).*/select * from DimProduct;-- a) e b)declare @desconto_economy float = 0.05;
+declare @desconto_regular float = 0.07;
+declare @desconto_deluxe float = 0.09;
+
+select
+	p.ProductKey,
+	p.ProductName,
+	p.Desconto,
+	p.UnitPrice * (1 - p.Desconto) as 'Preço com Desconto'
+from (
+	select
+		ProductKey,
+		ProductName,
+		UnitPrice,
+		case
+			when ClassName='Economy' then @desconto_economy
+			when ClassName='Regular' then @desconto_regular
+			when ClassName='Deluxe' then @desconto_deluxe
+			else 0.00
+		end as Desconto
+	from DimProduct
+) as p;
