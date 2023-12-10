198d3191e54284f079b926171c07bb2284e4408e
Jorge Harbes <jharbes@hotmail.com>
2023-12-10 09:59:07 -0300

    Update sqlserver-funcoes_condicionais-exercicios-resolucao.sql

--- 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
+++ 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
@@ -80,6 +80,7 @@ from DimProduct;
 
 
 
+
 /*
 2. Você ficou responsável pelo controle de produtos da empresa e deverá fazer uma análise da
 quantidade de produtos por Marca.
@@ -103,3 +104,33 @@ select
 	end as Categoria
 from DimProduct group by BrandName;
 
+
+
+
+/*
+3. Será necessário criar uma categorização de cada loja da empresa considerando a quantidade de
+funcionários de cada uma. A lógica a ser seguida será a lógica abaixo:
+
+EmployeeCount >= 50; 'Acima de 50 funcionários'
+EmployeeCount >= 40; 'Entre 40 e 50 funcionários'
+EmployeeCount >= 30; 'Entre 30 e 40 funcionários'
+EmployeeCount >= 20; 'Entre 20 e 30 funcionários'
+EmployeeCount >= 40; 'Entre 10 e 20 funcionários'
+Caso contrário: 'Abaixo de 10 funcionários'
+
+Faça uma consulta à tabela DimStore que retorne as seguintes informações: StoreName,
+EmployeeCount e a coluna de categoria, seguindo a regra acima.
+*/
+
+select
+	StoreName,
+	EmployeeCount,
+	case
+		when EmployeeCount >= 50 then 'Acima de 50 funcionários'
+		when EmployeeCount >= 40 then 'Entre 40 e 50 funcionários'
+		when EmployeeCount >= 30 then 'Entre 30 e 40 funcionários'
+		when EmployeeCount >= 20 then 'Entre 20 e 30 funcionários'
+		when EmployeeCount >= 10 then 'Entre 10 e 20 funcionários'
+		else 'Abaixo de 10 funcionários'
+		end as Categoria
+from DimStore;
