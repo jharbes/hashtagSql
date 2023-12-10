ac1ae713880fdd267f2334346839f8b7e9aa49c2
Jorge Harbes <jharbes@hotmail.com>
2023-12-10 14:00:11 -0300

    Create sqlserver-sql_views-exercicios-resolucao.sql

---
+++ 013-sqlserver-sql_views/sqlserver-sql_views-exercicios-resolucao.sql
@@ -0,0 +1,19 @@
+-- exercicios - sql_views
+
+use ContosoRetailDW;
+
+
+-- MÓDULO 13: EXERCÍCIOS
+
+/*
+1. a) A partir da tabela DimProduct, crie uma View contendo as informações de
+ProductName, ColorName, UnitPrice e UnitCost, da tabela DimProduct. Chame essa View
+de vwProdutos.
+
+b) A partir da tabela DimEmployee, crie uma View mostrando FirstName, BirthDate,
+DepartmentName. Chame essa View de vwFuncionarios.
+
+c) A partir da tabela DimStore, crie uma View mostrando StoreKey, StoreName e
+OpenDate. Chame essa View de vwLojas
+*/
+
