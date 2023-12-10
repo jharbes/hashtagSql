c9db986c1ead28f8f39da2843d849100f84159db
Jorge Harbes <jharbes@hotmail.com>
2023-12-10 09:44:56 -0300

    Update sqlserver-funcoes_condicionais-exercicios-resolucao.sql

--- 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
+++ 012-sqlserver-funcoes_condicionais/sqlserver-funcoes_condicionais-exercicios-resolucao.sql
@@ -18,7 +18,14 @@ ProductName, e outras duas colunas que deverão retornar o % de Desconto e UnitPr
 desconto.
 
 b) Faça uma adaptação no código para que os % de desconto de 5%, 7% e 9% sejam facilmente
-modificados (dica: utilize variáveis).*/select * from DimProduct;-- a) e b)declare @desconto_economy float = 0.05;
+modificados (dica: utilize variáveis).
+
+*/
+
+select * from DimProduct;
+
+-- a) e b)
+declare @desconto_economy float = 0.05;
 declare @desconto_regular float = 0.07;
 declare @desconto_deluxe float = 0.09;
 
@@ -70,3 +77,29 @@ select
 		else UnitPrice
 	end as 'Preço com Desconto'
 from DimProduct;
+
+
+
+/*
+2. Você ficou responsável pelo controle de produtos da empresa e deverá fazer uma análise da
+quantidade de produtos por Marca.
+
+A divisão das marcas em categorias deverá ser a seguinte:
+CATEGORIA A: Mais de 500 produtos
+CATEGORIA B: Entre 100 e 500 produtos
+CATEGORIA C: Menos de 100 produtos
+
+Faça uma consulta à tabela DimProduct e retorne uma tabela com um agrupamento de Total de
+Produtos por Marca, além da coluna de Categoria, conforme a regra acima.
+*/
+
+select 
+	BrandName as Marca,
+	count(BrandName) as 'Número de Produtos',
+	case
+		when count(BrandName) > 500 then 'CATEGORIA A'
+		when count(BrandName) < 100 then 'CATEGORIA C'
+		else 'CATEGORIA B'
+	end as Categoria
+from DimProduct group by BrandName;
+
