9a91b2aea39e04a63e718a5d3a9ead06d65b3124
Jorge Harbes <jharbes@hotmail.com>
2023-12-10 13:17:53 -0300

    Update 02-CREATE_VIEW-criando_a_primeira_view.sql

--- 013-sqlserver-sql_views/02-CREATE_VIEW-criando_a_primeira_view.sql
+++ 013-sqlserver-sql_views/02-CREATE_VIEW-criando_a_primeira_view.sql
@@ -1,6 +1,23 @@
 -- [SQL Server] Views
 -- Aula 2 de 13: CREATE VIEW - Criando a primeira View
 
+/*
+Quando falamos em Views, existem 3 açoes envolvidas:
+
+1- Criação de uma View;
+2- Alteração de uma View;
+3- Exclusão de uma View.
+
+Para cada uma dessas ações temos um comando associado:
+
+1- CREATE VIEW
+2- ALTER VIEW
+3- DROP VIEW
+
+*/
+
+-- CREATE VIEW
+
 -- 1. Exemplos
 -- a) Crie uma view contendo as seguintes informações da tabela DimCustomer: FirstName, EmailAddress e BirthDate. Chame essa view de vwClientes
 
@@ -10,7 +27,9 @@ SELECT
 	EmailAddress AS 'E-mail',
 	BirthDate AS 'Data Nascimento'
 FROM 
-	DimCustomer
+	DimCustomer;
+
+select * from vwClientes;
 
 -- b) Crie uma View contendo as seguintes informações da tabela DimProduct: ProductKey, ProductName, BrandName e UnitPrice. Chame essa view de vwProdutos
 
@@ -21,4 +40,6 @@ SELECT
 	ProductSubcategoryKey AS 'ID Subcategoria',
 	BrandName AS 'Marca',
 	UnitPrice AS 'Preço Unitário'
-FROM DimProduct
+FROM DimProduct;
+
+select * from vwProdutos;
