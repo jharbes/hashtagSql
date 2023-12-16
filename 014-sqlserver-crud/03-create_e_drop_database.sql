892ecee1c58a460705fa5b3a759e6ae26c4a90d8
Jorge Harbes <jharbes@hotmail.com>
2023-12-16 16:10:13 -0300

    Create 03-create_e_drop_database.sql

---
+++ 014-sqlserver-crud/03-create_e_drop_database.sql
@@ -0,0 +1,11 @@
+-- Criando e deletando nosso primeiro banco de dados
+
+-- criando banco de dados com nome teste1
+create database teste1;
+
+-- deletando banco de dados com nome teste1
+drop database teste1;
+
+
+-- criando banco de dados para utilizacao no curso
+create database BDImpressionador;
