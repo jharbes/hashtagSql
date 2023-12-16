f3c5a587b7c6e7551471355c36e12b2bf0015728
Jorge Harbes <jharbes@hotmail.com>
2023-12-16 17:41:30 -0300

    Update 07-INSERT_SELECT-adicionando_dados_de_outra_tabela.sql

--- 014-sqlserver-crud/07-INSERT_SELECT-adicionando_dados_de_outra_tabela.sql
+++ 014-sqlserver-crud/07-INSERT_SELECT-adicionando_dados_de_outra_tabela.sql
@@ -2,21 +2,21 @@
 -- Aula 7 de 21 - INSERT SELECT - Adicionando dados de outra tabela
 
 -- Crie uma tabela chamada 'Produtos'
--- Essa abela deve conter 4 colunas: id_produt, nome_produto, data_validade e preco_produto
+-- Essa tabela deve conter 4 colunas: id_produt, nome_produto, data_validade e preco_produto
 -- Certifique-se de que o tipo das colunas está correto.
 
-CREATE DATABASE BDImpressionador    -- Obs: Se você já criou o BD Impressionador não precisa executar esta linha
+CREATE DATABASE BDImpressionador;  -- Obs: Se você já criou o BD Impressionador não precisa executar esta linha
 
-USE BDImpressionador
+USE BDImpressionador;
 
 CREATE TABLE Produtos(
 	id_produto INT,
 	nome_produto VARCHAR(200),
 	data_validade DATETIME,
 	preco_produto FLOAT
-)
+);
 
-SELECT * FROM Produtos
+SELECT * FROM Produtos;
 
 -- Adicionando valores de outra tabela
 
@@ -24,9 +24,9 @@ INSERT INTO Produtos(id_produto, nome_produto, data_validade, preco_produto)
 SELECT
 	ProductKey,
 	ProductName,
-	AvailableForSaleDate,
+	AvailableForSaleDate, -- coluna escolhida aleatoriamente pois nao possui o mesmo significado de data_validade
 	UnitPrice
 FROM
-	ContosoRetailDW.dbo.DimProduct
+	ContosoRetailDW.dbo.DimProduct;
 
 
