ebb3b287b5c730058ff2afc54a6e325d22c2f260
Jorge Harbes <jharbes@hotmail.com>
2023-12-16 17:42:23 -0300

    Update sqlserver-sql_views-exercicios-resolucao.sql

--- 013-sqlserver-sql_views/sqlserver-sql_views-exercicios-resolucao.sql
+++ 013-sqlserver-sql_views/sqlserver-sql_views-exercicios-resolucao.sql
@@ -99,12 +99,85 @@ e DepartmentName. Chame essa de vwFuncionariosMkt.
 c) Crie uma View de uma tabela que considera apenas os produtos das marcas Contoso e
 Litware. Além disso, a sua View deve considerar apenas os produtos de cor Silver. Faça
 um SELECT de todas as colunas da tabela DimProduct. Chame essa View de
-vwContosoLitwareSilver.*/-- a)select * from DimStore;gocreate view vwLojasAtivas asselect 	*from DimStorewhere CloseDate is null;goselect * from vwLojasAtivas;-- b)gocreate view vwFuncionariosMkt asselect	FirstName,	EmailAddress,	DepartmentNamefrom DimEmployeewhere DepartmentName='Marketing';goselect * from vwFuncionariosMkt;-- c)gocreate view vwContosoLitwareSilver asselect	*from DimProductwhere BrandName in ('Contoso','Litware') and ColorName='Silver';goselect * from vwContosoLitwareSilver;/*4. Crie uma View que seja o resultado de um agrupamento da tabela FactSales. Este
+vwContosoLitwareSilver.
+*/
+
+-- a)
+select * from DimStore;
+
+go
+create view vwLojasAtivas as
+select 
+	*
+from DimStore
+where CloseDate is null;
+go
+
+select * from vwLojasAtivas;
+
+
+-- b)
+
+go
+
+create view vwFuncionariosMkt as
+select
+	FirstName,
+	EmailAddress,
+	DepartmentName
+from DimEmployee
+where DepartmentName='Marketing';
+
+go
+
+select * from vwFuncionariosMkt;
+
+
+-- c)
+
+
+go
+create view vwContosoLitwareSilver as
+select
+	*
+from DimProduct
+where BrandName in ('Contoso','Litware') and ColorName='Silver';
+go
+
+
+select * from vwContosoLitwareSilver;
+
+
+
+
+/*
+4. Crie uma View que seja o resultado de um agrupamento da tabela FactSales. Este
 agrupamento deve considerar o SalesQuantity (Quantidade Total Vendida) por Nome do
 Produto. Chame esta View de vwTotalVendidoProdutos.
 
 OBS: Para isso, você terá que utilizar um JOIN para relacionar as tabelas FactSales e
-DimProduct.*/select top(10) * from FactSales;gocreate view vwTotalVendidoProdutos asselect	ProductName as 'Nome do Produto',	SUM(SalesQuantity) as 'Quantidade Total Vendida'from FactSalesleft join DimProduct on FactSales.ProductKey=DimProduct.ProductKeygroup by ProductName;goselect * from vwTotalVendidoProdutos;/*5. Faça as seguintes alterações nas tabelas da questão 1.
+DimProduct.
+*/
+
+select top(10) * from FactSales;
+
+go
+create view vwTotalVendidoProdutos as
+select
+	ProductName as 'Nome do Produto',
+	SUM(SalesQuantity) as 'Quantidade Total Vendida'
+from FactSales
+left join DimProduct on FactSales.ProductKey=DimProduct.ProductKey
+group by ProductName;
+go
+
+select * from vwTotalVendidoProdutos;
+
+
+
+
+/*
+5. Faça as seguintes alterações nas tabelas da questão 1.
 
 a. Na View criada na letra a da questão 1, adicione a coluna de BrandName.
 b. Na View criada na letra b da questão 1, faça um filtro e considere apenas os
