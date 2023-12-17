cd64e1702c8a787c4477baff7dabe2c42b7076c9
Jorge Harbes <jharbes@hotmail.com>
2023-12-17 09:49:07 -0300

    Update 14-ALTER_TABLE-alterando_configuracoes_de_uma_tabela.sql

--- 014-sqlserver-crud/14-ALTER_TABLE-alterando_configuracoes_de_uma_tabela.sql
+++ 014-sqlserver-crud/14-ALTER_TABLE-alterando_configuracoes_de_uma_tabela.sql
@@ -7,22 +7,24 @@
 -- Adicionar coluna:
 
 ALTER TABLE Funcionarios
-ADD cargo varchar(100), bonus decimal(10,2)
+ADD cargo varchar(100), bonus decimal(10,2);
 
 UPDATE Funcionarios
-SET cargo = 'Analista'
-WHERE id_funcionario = 1
-SELECT * FROM Funcionarios
+SET cargo = 'Analista', bonus = 0.15
+WHERE id_funcionario = 1;
+
+
+SELECT * FROM Funcionarios;
 
 
 -- Alterar tipo de dados de uma coluna:
 
 ALTER TABLE Funcionarios
-ALTER COLUMN salario int
+ALTER COLUMN salario int;
 
 
 -- Deletar coluna:
 
 
 ALTER TABLE Funcionarios
-DROP COLUMN cargo, bônus
+DROP COLUMN cargo, bonus;
