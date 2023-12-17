5b48ed738bef05655422ce7515f4d540fd496327
Jorge Harbes <jharbes@hotmail.com>
2023-12-17 10:01:22 -0300

    Create sqlserver-sql_crud-exercicios-resolucao.sql

---
+++ 014-sqlserver-crud/sqlserver-sql_crud-exercicios-resolucao.sql
@@ -0,0 +1,17 @@
+-- exercicios - sql_views
+
+use BDImpressionador;
+
+
+-- MÓDULO 14: EXERCÍCIOS
+
+/*
+1. a) Crie um banco de dados chamado BD_Teste.
+
+b) Exclua o banco de dados criado no item anterior.
+
+c) Crie um banco de dados chamado Exercicios.
+*/
+
+-- a)
+
