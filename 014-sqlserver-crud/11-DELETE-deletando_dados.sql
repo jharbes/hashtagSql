0a9d46b06a98892bd245d4af64e0e7241dee1aad
Jorge Harbes <jharbes@hotmail.com>
2023-12-17 09:23:11 -0300

    Create 11-DELETE-deletando_dados.sql

---
+++ 014-sqlserver-crud/11-DELETE-deletando_dados.sql
@@ -0,0 +1,51 @@
+-- [SQL Server] CRUD
+-- Aula 11 de 21 - DELETE - Deletando dados
+
+-- Crie uma tabela chamada 'Produtos'
+-- Essa abela deve conter 4 colunas: id_produt, nome_produto, data_validade e preco_produto
+-- Certifique-se de que o tipo das colunas está correto.
+
+CREATE DATABASE BDImpressionador    -- Obs: Se você já criou o BD Impressionador não precisa executar esta linha
+
+USE BDImpressionador
+
+CREATE TABLE Produtos(
+	id_produto INT,
+	nome_produto VARCHAR(200),
+	data_validade DATETIME,
+	preco_produto FLOAT
+)
+
+SELECT * FROM Produtos
+
+-- Adicionando valores de outra tabela
+
+INSERT INTO Produtos(id_produto, nome_produto, data_validade, preco_produto)
+SELECT
+	ProductKey,
+	ProductName,
+	AvailableForSaleDate,
+	UnitPrice
+FROM
+	ContosoRetailDW.dbo.DimProduct
+
+-- Adicionando novos valores na tabela
+
+INSERT INTO Produtos(id_produto, nome_produto, data_validade, preco_produto)
+VALUES
+	(1, 'Arroz', '2021-12-31', 22.50),
+	(2, 'Feijão', '2021-12-31', 8.99)
+
+-- Atualizando o dado de uma tabela
+
+UPDATE Produtos
+SET nome_produto = 'Macarrão'
+WHERE id_produto = 3
+
+
+-- Deletando dados de uma tabela
+
+DELETE 
+FROM Produtos
+WHERE id_produto = 3
+
