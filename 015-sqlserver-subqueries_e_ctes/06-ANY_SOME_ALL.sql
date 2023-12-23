7727c5bd7e3a43a8d62e070e71a29a2062e67611
Jorge Harbes <jharbes@hotmail.com>
2023-12-23 17:35:02 -0300

    Update 06-ANY_SOME_ALL.sql

--- 015-sqlserver-subqueries_e_ctes/06-ANY_SOME_ALL.sql
+++ 015-sqlserver-subqueries_e_ctes/06-ANY_SOME_ALL.sql
@@ -4,7 +4,7 @@ CREATE TABLE funcionarios(
 id_funcionario INT,
 nome VARCHAR(50),
 idade INT,
-sexo VARCHAR(50))
+sexo VARCHAR(50));
 
 INSERT INTO funcionarios(id_funcionario, nome, idade, sexo)
 VALUES	
@@ -13,56 +13,82 @@ VALUES
 	(3, 'Amanda', 22, 'F'),
 	(4, 'Pedro', 23, 'M'),
 	(5, 'André', 24, 'M'),
-	(6, 'Luisa', 25, 'F')
+	(6, 'Luisa', 25, 'F');
 
-SELECT * FROM funcionarios
+SELECT * FROM funcionarios;
 
 -- Selecione os funcionários do sexo masculino (MAS, utilizando a coluna de IDADE para isso)
 
 SELECT * FROM funcionarios
-WHERE idade IN (21, 23, 24)
+WHERE idade IN (21, 23, 24);
 
 SELECT * FROM funcionarios
-WHERE idade IN (SELECT idade FROM funcionarios WHERE sexo = 'M')
+WHERE idade IN (SELECT idade FROM funcionarios WHERE sexo = 'M');
+
+
 
 /*
-= ANY(valor1, valor2, valor3) :
-Equivalente ao IN, retorna as lunhas da tabela que sejam iguais ao valor1, OU valor2, OU valor3
+= ANY ou SOME(valor1, valor2, valor3) :
+Equivalente ao IN, retorna as linhas da tabela que sejam iguais ao valor1, OU valor2, OU valor3
 */
+-- resultado: 21, 23 e 24
 
 SELECT * FROM funcionarios
-WHERE idade = ANY (SELECT idade FROM funcionarios WHERE sexo = 'M')
+WHERE idade = ANY (SELECT idade FROM funcionarios WHERE sexo = 'M');
+-- ou
+-- WHERE idade = SOME (SELECT idade FROM funcionarios WHERE sexo = 'M');
+
+
+
 
 /*
-> ANY(valor1, valor2, valor3) :
+> ANY ou SOME(valor1, valor2, valor3) :
 Retorna as linhas da tabela com valores maiores que o valor1, OU valor2, OU valor3. Ou seja, maior que o mínimo dos valores
 */
+-- resultado: 21, 23 e 24
+-- ou seja, todos maiores que 21
 
 SELECT * FROM funcionarios
-WHERE idade > ANY (SELECT idade FROM funcionarios WHERE sexo = 'M')
+--WHERE idade > ANY (SELECT idade FROM funcionarios WHERE sexo = 'M');
+-- ou
+WHERE idade > SOME (SELECT idade FROM funcionarios WHERE sexo = 'M');
+
+
+
 
 /*
-< ANY(valor1, valor2, valor3) :
-Retorna as linhas da tabela com valores maiores que o valor1, OU valor2, OU valor3. Ou seja, maior que o máximo dos valores
+< ANY ou SOME(valor1, valor2, valor3) :
+Retorna as linhas da tabela com valores maiores que o valor1, OU valor2, OU valor3. Ou seja, menor que o máximo dos valores
 */
+-- resultado: 21, 23 e 24
+-- ou seja, todos menores que 24
 
 SELECT * FROM funcionarios
-WHERE idade < ANY (SELECT idade FROM funcionarios WHERE sexo = 'M')
+WHERE idade < ANY (SELECT idade FROM funcionarios WHERE sexo = 'M');
+-- ou
+-- WHERE idade < SOME (SELECT idade FROM funcionarios WHERE sexo = 'M');
+
 
 
 /*
 > ALL(valor1, valor2, valor3) :
 Retorna as linhas da tabela com valores maiores que o valor1, E valor2, E valor3. Ou seja, maior que o máximo dos valores
 */
+-- resultado: 21, 23 e 24
+-- ou seja, todos maiores que 24
 
 SELECT * FROM funcionarios
-WHERE idade > ALL (SELECT idade FROM funcionarios WHERE sexo = 'M')
+WHERE idade > ALL (SELECT idade FROM funcionarios WHERE sexo = 'M');
+
+
 
 /*
 < ALL(valor1, valor2, valor3) :
 Retorna as linhas da tabela com valores menores que o valor1, E valor2, E valor3. Ou seja, menor que o mínimo dos valores
 */
+-- resultado: 21, 23 e 24
+-- ou seja, todos menores que 21
 
 SELECT * FROM funcionarios
-WHERE idade < ALL (SELECT idade FROM funcionarios WHERE sexo = 'M')
+WHERE idade < ALL (SELECT idade FROM funcionarios WHERE sexo = 'M');
 
