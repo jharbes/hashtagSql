973ad2e825188faf850e56e098fd7e3466bab616
Jorge Harbes <jharbes@hotmail.com>
2023-12-24 13:57:04 -0300

    Update 09-subquery_na_pratica-aplicacao_com_o_select.sql

--- 015-sqlserver-subqueries_e_ctes/09-subquery_na_pratica-aplicacao_com_o_select.sql
+++ 015-sqlserver-subqueries_e_ctes/09-subquery_na_pratica-aplicacao_com_o_select.sql
@@ -2,9 +2,26 @@
 
 -- Exemplo: Retornar uma tabela com todos os produtos (ID Produto e Nome Produto) e também o total de vendas para cada produto
 
+
 SELECT
 	ProductKey,
 	ProductName,
 	(SELECT COUNT(ProductKey) FROM FactSales WHERE FactSales.ProductKey = DimProduct.ProductKey) AS 'Qtd. Vendas'
 FROM
-	DimProduct
+	DimProduct;
+
+
+
+select
+	FactSales.ProductKey,
+	DimProduct.ProductName,
+	count(FactSales.ProductKey) as 'Número de Vendas'
+from
+	FactSales
+left join DimProduct on FactSales.ProductKey=DimProduct.ProductKey
+group by FactSales.ProductKey, DimProduct.ProductName
+order by FactSales.ProductKey;
+
+
+select distinct ProductKey from FactSales
+order by ProductKey;
