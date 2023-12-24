6d887a080490dd9060b7a22f2b0c3a47142c1597
Jorge Harbes <jharbes@hotmail.com>
2023-12-24 15:22:15 -0300

    Update 11-plano_de_execucao_estimado.sql

--- 015-sqlserver-subqueries_e_ctes/11-plano_de_execucao_estimado.sql
+++ 015-sqlserver-subqueries_e_ctes/11-plano_de_execucao_estimado.sql
@@ -2,11 +2,16 @@
 
 -- Exemplo: Retornar a quantidade total de produtos da marca Contoso.
 
+-- Ao utilizar o "exibir plano de execução estimado" (botão no menu superior ou CTRL + L)
+-- podemos saber qual das consultas tem menor custo de consulta , ou seja, é mais otimizado
+-- podendo decidir entao qual é o melhor de ser utilizado
+-- MENOR CUSTO = MELHOR OPÇÃO
+
 SELECT 
 	COUNT(*) 
 FROM DimProduct
-WHERE BrandName = 'Contoso'
+WHERE BrandName = 'Contoso';
 
 SELECT
 	COUNT(*)
-FROM (SELECT * FROM DimProduct WHERE BrandName = 'Contoso') AS T
+FROM (SELECT * FROM DimProduct WHERE BrandName = 'Contoso') AS T;
