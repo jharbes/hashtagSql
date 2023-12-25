a40f0f3b3e9327eff1cb95bca0de5ad4acb8fea9
Jorge Harbes <jharbes@hotmail.com>
2023-12-25 06:28:56 -0300

    Update 17-criando_multiplas_CTEs.sql

--- 015-sqlserver-subqueries_e_ctes/17-criando_multiplas_CTEs.sql
+++ 015-sqlserver-subqueries_e_ctes/17-criando_multiplas_CTEs.sql
@@ -1,7 +1,7 @@
 -- [SQL Server] [Subqueries e CTE's] Aula 17 de 28: Criando múltiplas CTE's
 
 -- Exemplo: Crie 2 CTE's:
--- 1. A primeira, cjamada produtos_contoso, deve conter as seguintes colunas (DimProduct): ProductKey, ProductName, BrandName
+-- 1. A primeira, chamada produtos_contoso, deve conter as seguintes colunas (DimProduct): ProductKey, ProductName, BrandName
 -- 2. A segunda, chamada vendas_top100, deve ser um top 100 vendas mais recentes, considerando as seguintes colunas da tabela FactSales: SalesKey, ProductKey, DateKey, SalesQuantity
 
 -- Por fim, faça um INNER JOIN dessas tabelas
@@ -30,4 +30,4 @@ vendas_top100 AS (
 
 SELECT * FROM vendas_top100
 INNER JOIN produtos_contoso
-	ON vendas_top100.ProductKey = produtos_contoso.ProductKey
+	ON vendas_top100.ProductKey = produtos_contoso.ProductKey;
