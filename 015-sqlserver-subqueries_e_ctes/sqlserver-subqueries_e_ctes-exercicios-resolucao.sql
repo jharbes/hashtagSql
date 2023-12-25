c2c5475fac96761b98e5c50763bda7b9fadd5246
Jorge Harbes <jharbes@hotmail.com>
2023-12-25 08:24:01 -0300

    Create sqlserver-subqueries_e_ctes-exercicios-resolucao.sql

---
+++ 015-sqlserver-subqueries_e_ctes/sqlserver-subqueries_e_ctes-exercicios-resolucao.sql
@@ -0,0 +1,12 @@
+-- exercicios - subqueries_e_ctes
+
+
+-- MÓDULO 15: EXERCÍCIOS
+
+/*
+1. Para fins fiscais, a contabilidade da empresa precisa de uma tabela contendo todas as vendas
+referentes à loja ‘Contoso Orlando Store’. Isso porque essa loja encontra-se em uma região onde
+a tributação foi modificada recente.
+
+Portanto, crie uma consulta ao Banco de Dados para obter uma tabela FactSales contendo todas
+as vendas desta loja.*/
