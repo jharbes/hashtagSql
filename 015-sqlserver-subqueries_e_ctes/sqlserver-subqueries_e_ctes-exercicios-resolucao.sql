2731cecea33389e98942c0fae232311aae41e310
Jorge Harbes <jharbes@hotmail.com>
2023-12-25 11:14:34 -0300

    Update sqlserver-subqueries_e_ctes-exercicios-resolucao.sql

--- 015-sqlserver-subqueries_e_ctes/sqlserver-subqueries_e_ctes-exercicios-resolucao.sql
+++ 015-sqlserver-subqueries_e_ctes/sqlserver-subqueries_e_ctes-exercicios-resolucao.sql
@@ -195,3 +195,22 @@ with CTE_QtdProdutosPorMarca as (
 )
 
 select MAX(qt_produtos_marca) as 'Máximo Produtos por Marca' from CTE_QtdProdutosPorMarca;
+
+
+
+
+/*
+10. Crie duas CTEs:
+
+(i) a primeira deve conter as colunas ProductKey, ProductName, ProductSubcategoryKey,
+BrandName e UnitPrice, da tabela DimProduct, mas apenas os produtos da marca Adventure
+Works. Chame essa CTE de CTE_ProdutosAdventureWorks.
+
+(ii) a segunda deve conter as colunas ProductSubcategoryKey, ProductSubcategoryName, da
+tabela DimProductSubcategory mas apenas para as subcategorias ‘Televisions’ e ‘Monitors’.
+Chame essa CTE de CTE_CategoriaTelevisionsERadio.
+
+Faça um Join entre essas duas CTEs, e o resultado deve ser uma query contendo todas as colunas
+das duas tabelas. Observe nesse exemplo a diferença entre o LEFT JOIN e o INNER JOIN.
+*/
+
