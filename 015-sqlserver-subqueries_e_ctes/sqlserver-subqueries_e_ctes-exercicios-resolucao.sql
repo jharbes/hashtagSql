529e611ca049f75e15eb094aeb10a79613deabe4
Jorge Harbes <jharbes@hotmail.com>
2023-12-25 11:03:45 -0300

    Update sqlserver-subqueries_e_ctes-exercicios-resolucao.sql

--- 015-sqlserver-subqueries_e_ctes/sqlserver-subqueries_e_ctes-exercicios-resolucao.sql
+++ 015-sqlserver-subqueries_e_ctes/sqlserver-subqueries_e_ctes-exercicios-resolucao.sql
@@ -176,3 +176,13 @@ select
 	AVG(Quantidade) as 'Média de produtos por marca'
 from
 	(select COUNT(ProductKey) as Quantidade from DimProduct group by BrandName) as T;
+
+
+
+
+/*
+9. Crie uma CTE que seja o agrupamento da tabela DimProduct, armazenando o total de
+produtos por marca. Em seguida, faça um SELECT nesta CTE, descobrindo qual é a quantidade
+máxima de produtos para uma marca. Chame esta CTE de CTE_QtdProdutosPorMarca.
+*/
+
