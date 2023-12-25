acef1affa6ef72647c75ff6228285b35e953b32f
Jorge Harbes <jharbes@hotmail.com>
2023-12-25 06:09:10 -0300

    Update 14-CTE-o_que_eh_e_como_criar.sql

--- 015-sqlserver-subqueries_e_ctes/14-CTE-o_que_eh_e_como_criar.sql
+++ 015-sqlserver-subqueries_e_ctes/14-CTE-o_que_eh_e_como_criar.sql
@@ -1,5 +1,10 @@
 -- [SQL Server] [Subqueries e CTE's] Aula 14 de 28: CTE - O que é e como criar
 
+
+-- CTE: Common Table Expression
+
+-- Funciona como uma tabela TEMPORÁRIA para ser utilizada em expressões seguintes
+
 -- Exemplo: Crie uma CTE para armazenar o resultado de uma consulta que contenha: ProductKey, ProductName, BrandName, ColorName e UnitPrice, apenas para a marca Contoso.
 
 WITH cte AS (
@@ -13,6 +18,8 @@ FROM DimProduct
 WHERE BrandName = 'Contoso'
 )
 
+
+
 SELECT
 	COUNT(*)
-FROM cte
+FROM cte;
