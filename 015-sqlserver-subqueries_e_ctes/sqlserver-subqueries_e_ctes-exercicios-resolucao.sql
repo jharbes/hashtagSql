9cb156759fb63c05b4776895aa44472a2abbae93
Jorge Harbes <jharbes@hotmail.com>
2023-12-25 08:35:38 -0300

    Update sqlserver-subqueries_e_ctes-exercicios-resolucao.sql

--- 015-sqlserver-subqueries_e_ctes/sqlserver-subqueries_e_ctes-exercicios-resolucao.sql
+++ 015-sqlserver-subqueries_e_ctes/sqlserver-subqueries_e_ctes-exercicios-resolucao.sql
@@ -9,4 +9,15 @@ referentes à loja ‘Contoso Orlando Store’. Isso porque essa loja encontra-se em 
 a tributação foi modificada recente.
 
 Portanto, crie uma consulta ao Banco de Dados para obter uma tabela FactSales contendo todas
-as vendas desta loja.*/select	*from	FactSaleswhere StoreKey=(select StoreKey from DimStore where StoreName='Contoso Orlando Store');
+as vendas desta loja.*/-- para utilizacao do WHERE a resposta da subquery deve ser um valor unico conforma abaixoselect	*from	FactSaleswhere StoreKey=(select StoreKey from DimStore where StoreName='Contoso Orlando Store');/*2. O setor de controle de produtos quer fazer uma análise para descobrir quais são os produtos
+que possuem um UnitPrice maior que o UnitPrice do produto de ID igual a 1893.
+
+a) A sua consulta resultante deve conter as colunas ProductKey, ProductName e UnitPrice
+da tabela DimProduct.
+
+b) Nessa query você também deve retornar uma coluna extra, que informe o UnitPrice do
+produto 1893.
+*/
+
+-- a)
+
